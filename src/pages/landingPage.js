7623fb9d5e5be609c0266d52f22b678762e747ba
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-06-10 23:47:41 +0000

    created landingPage.js, added Header component

---
+++ src/pages/landingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// components
+import Header from "../components/header";
+
+const LandingPage = () => {
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+};
+
+export default LandingPage;
