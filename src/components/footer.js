d96338f213c00580f54033cc7bfdfe918ab74ec4
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-06-11 00:07:48 +0000

    created footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaGithub } from 'react-icons/fa';
+import logo from "../assets/logo.png";
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            
+            <img src={logo} alt="brand logo" className="logo"/>
+
+            <div className="links-container">
+                <div className="github-repos">
+                    <h3>DEVELOPMENT</h3>
+                    <a href="https://github.com/Team-BookIt/BookIt-Client">Frontend</a>
+                    <a href="https://github.com/Team-BookIt/BookIt-Server">Backend</a>
+                </div>
+                <div className="documentation">
+                    <h3>DOCUMENTATION</h3>
+                    <a href="https://www.figma.com/design/naKB2JfKyjKQSekl61tGHd/BookIt%2CUI%2FUX?node-id=76-2&t=Q2ZdQMg2236jtFlb-0">UI plan</a>
+                    <a href="https://www.figma.com/design/naKB2JfKyjKQSekl61tGHd/BookIt%2CUI%2FUX?node-id=76-2&t=Q2ZdQMg2236jtFlb-0">Requirements</a>
+                </div>
+            </div>
+
+            <FaGithub size={80}/>
+        </div>
+    );
+};
+
+export default Footer;
