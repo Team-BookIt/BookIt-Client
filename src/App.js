30e4a96404cf21c5181424d0205f0a69efe2651a
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-06-18 00:55:53 +0000

    general updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 // components
 import LandingPage from './pages/landingPage';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      {/* <Login /> */}
+      {/* <Signup /> */}
     </div>
   );
 }
