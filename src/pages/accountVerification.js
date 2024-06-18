836f3a6763b8419b772c07a02ea0013785580127
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-06-18 23:56:50 +0000

    organizer profile completion and account verification

---
+++ src/pages/accountVerification.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import PrimaryButton from "../components/landing page/primaryButton";
+
+import logo from "../assets/logo.png";
+import sideImage from "../assets/onboarding-1.png";
+
+
+
+const AccountVerification = () => {
+    const navigate = useNavigate();
+    
+    const handleVerifyPress = () => {
+        navigate("/");
+    }
+    return(
+        <div className="onboarding-container">
+            <img src={sideImage} className="onboarding-img-container" />
+
+            <div className="onboarding-main">
+                <img src={logo} />
+                <p>
+                    Account Verification.
+                </p>
+
+                <div className="verification-text-container">
+                    <p>
+                        We've sent a verification email to *****@gmail.com.
+                    </p>
+                    
+                    <p>
+                        Kindly enter it in the field below.
+                    </p>
+                </div>
+
+
+                <div className="form-container">
+                    <input type="text" name="verificationCode" placeholder="5-digit code" required/>
+                </div>
+
+                <div className="verification-footer">
+                    <p>
+                        Didn't get the code? <br/> Let us <a href="#">resend</a>
+                    </p>
+                    
+                    <PrimaryButton title={"Verify"} onButtonClick={handleVerifyPress}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AccountVerification;
---
+++ src/pages/organizerProfileFrom.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import PrimaryButton from "../components/landing page/primaryButton";
+
+import logo from "../assets/logo.png";
+import sideImage from "../assets/onboarding-1.png";
+
+
+
+const OrganizerProfileForm = () => {
+    const navigate = useNavigate();
+    
+    const handleNextPress = () => {
+        navigate("/accountVerification")
+    }
+
+    return(
+        <div className="onboarding-container">
+            <img src={sideImage} className="onboarding-img-container" />
+
+            <div className="onboarding-main">
+                <img src={logo} />
+                <p>
+                    Organizer Profile Completion.
+                </p>
+
+
+                <div className="form-container">
+                    <input type="text" name="companyName" placeholder="Company/Organizer Name" required/>
+                    <input type="text" name="companyWebsite" placeholder="Company website link" required/>
+                    <input type="email" name="companyEmail" placeholder="Company email address" required/>
+                    <input type="text" name="companyDescription" placeholder="Tell us a bit about the company..." required/>
+                    <input type="text" name="companyLogo" placeholder="Upload company logo" required/>
+                </div>
+                
+                <PrimaryButton title={"Next"} onButtonClick={handleNextPress}/>
+            </div>
+        </div>
+    );
+}
+
+export default OrganizerProfileForm;
