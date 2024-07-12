b790c4eace86101f1d384567599c28d2de451db3
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-07-12 12:03:10 +0000

    Updated to get booked events from backend

--- src/pages/(attendee)/bookings.js
+++ src/pages/(attendee)/bookings.js
@@ -1,34 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../../components/(universal)/searchBar";
 import BookedEvent from "../../components/(attendee)/bookedEvent";
+import { getUserBookedEvents } from "../../utils/getUserBookedEvents";
 
 const Bookings = () => {
+    const [bookedEvents, setBookedEvents] = useState({});
+
+    useEffect(() => {
+        setBookedEvents(getUserBookedEvents);
+    }, [])
     return (
         <div className="page-container">
             <p>Events you've signed up for</p>
             <SearchBar />
 
             <div className="events-container">
-                <BookedEvent 
-                    name={"Codefest"} 
-                    venue={"Kumapley Auditorium, KNUST"}
-                    daysLeft={"3"}
-                />
-                <BookedEvent 
-                    name={"Codefest"} 
-                    venue={"Kumapley Auditorium, KNUST"}
-                    daysLeft={"3"}
-                />
-                <BookedEvent 
-                    name={"Codefest"} 
-                    venue={"Kumapley Auditorium, KNUST"}
-                    daysLeft={"3"}
-                />
-                <BookedEvent 
-                    name={"Codefest"} 
-                    venue={"Kumapley Auditorium, KNUST"}
-                    daysLeft={"3"}
-                />
+                {bookedEvents && bookedEvents.map(() => (
+                    <BookedEvent />
+                ))}
             </div>
         </div>
     );
