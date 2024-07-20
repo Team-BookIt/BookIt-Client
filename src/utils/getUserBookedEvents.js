21d12b6b11dbd8e2b8f7885622850320f7d726d2
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-07-20 02:42:27 +0000

    Fixed backend url error

--- src/utils/getUserBookedEvents.js
+++ src/utils/getUserBookedEvents.js
@@ -6,9 +6,9 @@ const backendURL = process.env.REACT_APP_BOOKIT_BACKEND_URL;
 export const getUserBookedEvents = async () => {
     const userID = getUserData().id;
     try {
-        const response = await axios.get(`${backendURL}/bookings/${userID}`);
+        const response = await axios.get(`${backendURL}/events/bookings/${userID}`);
         console.log(response.data);
-        return (response.data);
+        return (response.data.bookedEvents);
     } catch (error) {
         console.log("Error getting booked events:", error);
     }
