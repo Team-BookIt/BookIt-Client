38e98fd2810fecd18012a5d290007477dfa636b6
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-08-05 23:43:45 +0000

    Added user interest posting

--- src/utils/bookEvent.js
+++ src/utils/bookEvent.js
@@ -1,17 +1,28 @@
 import axios from "axios";
-import { getUserData } from "./getUserData";
 
 const backendURL = "https://book-it-server-sigma.vercel.app";
 
+const userID = JSON.parse(localStorage.getItem("user")).id;
 
 export const bookEvent = async (eventID) => {
-    const userID = getUserData().id;
+    console.log("User id", userID);
     try {
         const response = await axios.post(`${backendURL}/events/register`, {eventID, userID});
-        console.log(response.data);
+        console.log("Event booked?", response.data);
         return response.data;
     } catch (error) {
         console.log("Error booking event:", error);
     }
 };
+
+export const addUserInterests = async(interests) => {
+    const id = userID
+    try {
+        const response = await axios.post(`${backendURL}/profile/user/interests`, {interests, id});
+        console.log("User interests added:", response.data);
+        return response.data;
+    } catch (error) {
+        console.log("Error posting user interests", error);
+    }
+}
 
