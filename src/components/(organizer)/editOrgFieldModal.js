a19571878048d603cf107c77abc688c88b618034
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-08-05 23:41:25 +0000

    Added loading animation

--- src/components/(organizer)/editOrgFieldModal.js
+++ src/components/(organizer)/editOrgFieldModal.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { editOrganizerProfile } from "../../utils/editOrgProfile";
 import { MdClose } from "react-icons/md";
+import Loading from "../(universal)/loading";
 
 const EditFieldModal = ({ field, value, type, onClose }) => {
     const [fieldValue, setFieldValue] = useState(value);
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault(); 
+        setLoading(true);
         try {
             await editOrganizerProfile({ [field]: fieldValue }); 
             onClose(); 
         } catch (error) {
             console.error("Error updating organizer profile:", error);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -30,7 +35,7 @@ const EditFieldModal = ({ field, value, type, onClose }) => {
                             onChange={(e) => setFieldValue(e.target.value)}
                         />
 
-                        <button type="submit">Update {field}</button>
+                        <button type="submit">{loading ? <Loading /> : `Update ${field}`}</button>
                     </form>
                 </div>
             </div>
