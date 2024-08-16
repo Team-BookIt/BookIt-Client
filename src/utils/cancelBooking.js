27abd1bfd26026bbf1873d5a32b9b8e904c15c2d
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-08-16 17:31:49 +0000

    Updated backend route

--- src/utils/cancelBooking.js
+++ src/utils/cancelBooking.js
@@ -6,10 +6,24 @@ export const cancelBooking = async (eventID) => {
     const user = localStorage.getItem("user");
     const userID = JSON.parse(user).id;
 
+    const guestID = Number(userID);
+    console.log("Event id:", eventID);
+    console.log("Guest id:", guestID);
+
+    const dataToPass = {
+        guestID: guestID,
+        eventID: eventID,
+    }
+
     try {
-        const request = await axios.delete(`${backendURL}/events/bookings/delete`, {userID, eventID});
-        console.log("Booking cancellation status:", request.data);
+        const request = await axios.delete(`${backendURL}/events/bookings/delete/${guestID}/${eventID}`);
+        console.log("Booking cancellation status:", request.data.response);
+        return (request.data.response);
     } catch (error) {
         console.log("Error cancelling booking:", error);
+    } finally{
+        console.log("Guest id after request:", guestID);
+        console.log("Event id after request:", eventID);
     }
+
 }
