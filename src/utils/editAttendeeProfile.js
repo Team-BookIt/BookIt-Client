e9f02d3f6f548e935a4669af1350758c2d228084
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-08-16 17:31:25 +0000

    Updated backend route

--- src/utils/editAttendeeProfile.js
+++ src/utils/editAttendeeProfile.js
@@ -2,13 +2,14 @@ import axios from "axios"
 
 const backendURL = "https://book-it-server-sigma.vercel.app";
 
-export const editAttendeeProfile = async( field ) => {
-    const userId = JSON.parse(localStorage.getItem("user")).id;
+export const editAttendeeProfile = async( attributes ) => {
+    const userId = Number(JSON.parse(localStorage.getItem("user")).id);
+    console.log("Attributes", attributes);
 
     try {
-        const response = await axios.put(`${backendURL}/profile/user/update`, {field, userId});
+        const response = await axios.put(`${backendURL}/profile/user/${userId}/update`, {attributes});
         console.log("Profile update response:", response.data);
-        console.log("Field being updated:", field);
+        console.log("Field being updated:", attributes);
         return (response.data);
     } catch (error) {
         console.log("Error updating attendee profile:", error);
--- src/utils/editOrgProfile.js
+++ src/utils/editOrgProfile.js
@@ -1,10 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
 const backendURL = "https://book-it-server-sigma.vercel.app";
+const orgID = Number(JSON.parse(localStorage.getItem("organizer")).id);
 
 export const editOrganizerProfile = async( field ) => {
     try {
-        const response = await axios.put(`${backendURL}/profile/org/update`, field);
+        const response = await axios.put(`${backendURL}/profile/org/${orgID}/update`, field);
         console.log("Profile updated successfully!", response.data);
         return (response.data);
     } catch (error) {
