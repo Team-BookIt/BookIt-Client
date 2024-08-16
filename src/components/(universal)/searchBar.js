da6f382c2e57ca7ee8f25237d0461e524bd609e1
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-08-16 17:30:24 +0000

    Implemented search functionality

--- src/components/(universal)/searchBar.js
+++ src/components/(universal)/searchBar.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdSearch } from "react-icons/md";
 
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+        onSearch(e.target.value);
+    }
+
     return (
         <div className="search-bar">
             <input
                 placeholder="Search events by name, location, category... "
                 type="text"
+                value={searchTerm}
+                onChange={handleInputChange}
             />
             <MdSearch size={25}/>
         </div>
